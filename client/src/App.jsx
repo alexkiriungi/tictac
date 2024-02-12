def14af1436f16e48ad7d86b28149835626aea5f
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-12 13:00:03 +0300

    Routing pages complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Album from './pages/Album';
+import Login from './pages/Login';
+import SignUp from "./pages/SignUp";
+import Photo from './pages/Photo';
+import Profile from './pages/Profile';
 
 export default function App() {
   return (
-    <div>App</div>
-  )
+    
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/album' element={<Album />} />
+          <Route path='/log-in' element={<Login />} />
+          <Route path='/sign-up' element={<SignUp /> } />
+          <Route path='/photo' element={<Photo />} />
+          <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
