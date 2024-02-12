3dbda2499f6d8db094bbb730087e2ef246c2a82f
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-12 11:38:05 +0300

    Added the theme and user reducers using reduxjs/toolkit

---
+++ client/src/pages/Album.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Album() {
+  return (
+    <div>Album</div>
+  )
+}
---
+++ client/src/pages/Photo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Photo() {
+  return (
+    <div>Photo</div>
+  )
+}
