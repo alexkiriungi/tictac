f841c4dd112c4d2f196c46674edd44c4a9db3a4b
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-16 20:49:25 +0300

    Added the create album API route

--- api/controllers/album.controller.js
+++ api/controllers/album.controller.js
@@ -1,2 +1,8 @@
 import User from "../models/user.model.js";
 import Album from "../models/album.model.js";
+import multer from 'multer';
+
+
+export const createAlbum = async ( req, res, next) => {
+    
+};
--- api/routes/album.route.js
+++ api/routes/album.route.js
@@ -1,8 +1,22 @@
 import express from 'express';
+import { errorHandler } from '../utilis/error.js';
 import { verifyToken } from '../utilis/verifyUser.js';
+import multer from 'multer';
+import { createAlbum } from '../controllers/album.controller.js';
+import path from 'path';
 
 const router = express.Router();
 
-router.post('/create', verifyToken, createAlbum);
+const storage = multer.diskStorage({
+    destination: function( req, file, cb) {
+        cb( errorHandler(400, 'Error! Please try again!'), '../client/public/images' );
+    },
+    filename: (req, file, cb) => {
+        cb(errorHandler(400, 'Error! Please try again!'), Date.now() + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage});
+
+router.post('/create', verifyToken, upload.single('image'), createAlbum);
 
 export default router;
