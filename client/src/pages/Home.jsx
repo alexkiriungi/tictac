b6d21579d56665e86d559a968eba64c5cc7fbfa1
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-19 12:26:38 +0300

    final changes complete, ready for deployment

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,9 +6,11 @@ export default function Home() {
       <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto">
         <h1 className='txt-3xl font-bold lg:text-6xl'>Welcome to Tictac</h1>
         <p className='text-gray-500 text-xs sm:text-sm'>
-          Tictac is a file sharing platform where users get to share memories,
-          wallpapers, snaps and many more! Let us recreate your imagination
-          and make it real!
+          Your got-to destination for personalized and thoughtful
+          card postings! At Tictac we believe that every message is 
+          an opportunity to create a meaningful connection, and our
+          platform is designed to make expressing your sentiments easy,
+          delightful, and memorable!
         </p>
       </div>
       <AlbumCard />
