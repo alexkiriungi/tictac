826267e2c7e0175c825c46077ecf98744f81188a
Alex Kiriungi <134509644+alexkiriungi@users.noreply.github.com>
2024-02-21 11:54:43 +0300

    Adding the available cards components

---
+++ client/src/components/AlbumCards.jsx
@@ -0,0 +1,17 @@
+import { Card } from 'flowbite-react';
+
+export default function AlbumCards({ album }) {
+    return(
+        <div className='flex flex-wrap gap-4 justify-center p-2'>
+            <Card
+            className='max-w-sm'
+            imgAlt={album.title}
+            imgSrc={album.image} 
+            >
+                <h5 className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                    {album.title}
+                </h5>
+            </Card>
+        </div>
+    );
+}
